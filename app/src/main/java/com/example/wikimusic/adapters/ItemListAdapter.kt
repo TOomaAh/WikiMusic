693209f0456a2f473e228ec8199f4283b2b846ac
Antoine <antoine.thiel@hotmail.fr>
2021-07-21 15:35:53 +0200

    cells done go try adapter

--- app/src/main/java/com/example/wikimusic/adapters/ItemListAdapter.kt
+++ app/src/main/java/com/example/wikimusic/adapters/ItemListAdapter.kt
@@ -3,9 +3,15 @@ package com.example.wikimusic.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wikimusic.R
 import com.example.wikimusic.models.Album
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_album_cell.view.*
+import org.w3c.dom.Text
+import javax.security.auth.callback.Callback
 
 class ItemListAdapter<T>(private val items: List<T>) : RecyclerView.Adapter<ItemListViewHolder<T>>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemListViewHolder<T> {
@@ -14,7 +20,7 @@ class ItemListAdapter<T>(private val items: List<T>) : RecyclerView.Adapter<Item
                 parent,
                 false))
         }
-        return ItemListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_album_cell,
+        return ItemListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_artist_cell,
             parent,
             false))
     }
@@ -30,7 +36,41 @@ class ItemListAdapter<T>(private val items: List<T>) : RecyclerView.Adapter<Item
 
 
 class ItemListViewHolder<T>(v: View) : RecyclerView.ViewHolder(v){
-    fun updateItem(items: T){
+
+    //Album
+//    val thumbnail : ImageView = v.itemAlbumIcon
+//    val albumName: TextView = v.itemNameText
+//    val artistName: TextView = v.artist_name_album
+
+    //Artist
+//    val thumbnail: ImageView = v.itemAlbumIcon
+//    val artistName: TextView = v.itemNameText
+
+    fun updateItem(item: T){
+        //Album
+            //albumName.text = item.strAlbum
+            //artistName.text = item.strArtist
+//        Picasso.get().load(item.strAlbumThumb).into(thumbnail, object : Callback {
+//            override fun onSuccess() {
+//
+//            }
+//
+//            override fun onError(e: Exception?) {
+//
+//            }
+//        })
+
+        //Artist
+            //artistName.text = item.strArtist
+//        Picasso.get().load(item.strArtistThumb).into(thumbnail, object : Callback {
+//            override fun onSuccess() {
+//
+//            }
+//
+//            override fun onError(e: Exception?) {
+//
+//            }
+//        })
 
     }
 }
--- app/src/main/java/com/example/wikimusic/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/wikimusic/fragments/SearchFragment.kt
@@ -5,14 +5,69 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.wikimusic.R
+import com.example.wikimusic.adapters.ItemListAdapter
+import com.example.wikimusic.models.Album
+import com.example.wikimusic.models.Artist
+import kotlinx.android.synthetic.main.fragment_search.*
+import kotlinx.android.synthetic.main.fragment_search.view.*
 
 class SearchFragment : Fragment() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter : RecyclerView.Adapter<SearchAdapter.ViewHolder>? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        //Selon le prof
+//        view.recyclerArtistSearch.layoutManager = LinearLayoutManager(requireContext())
+//        view.recyclerArtistSearch.adapter = ItemListAdapter<Artist>(list_artsite);
+//
+//        view.recyclerRecordSearch.layoutManager = LinearLayoutManager(requireContext())
+//        view.recyclerRecordSearch.adapter = ItemListAdapter<Album>(list_album);
+
+
+
+        recyclerArtistSearch.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = SearchAdapter()
+        }
+        recyclerRecordSearch.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = SearchAdapter()
+        }
+    }
+}
+
+class SearchAdapter : RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_artist_cell, parent, false)
+        return ViewHolder(v);
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 5
     }
 }
